22fe7d02d693c4a378ca2676f901c978cf8dc044
Zubaer Alam <zobaeralam@iut-dhaka.edu>
2023-06-23 23:39:55 +0600

    Refactored app.js

--- Personal Website/app.js
+++ Personal Website/app.js
@@ -1,21 +1,25 @@
-function showDate() {
-  setInterval(function setTime() {
+const showDate = () => {
+  setInterval(() => {
     document.getElementById('time').innerHTML = Date();
   }, 1000);
-}
+};
 
-function showHomePage() {
-  document.getElementById('Homepage').style.display = 'block';
-  document.getElementById('About').style.display = 'none';
-  document.getElementById('contactInfo').style.display = 'none';
-}
-function showAboutPage() {
-  document.getElementById('Homepage').style.display = 'none';
-  document.getElementById('About').style.display = 'block';
-  document.getElementById('contactInfo').style.display = 'none';
-}
-function showContactInfo() {
-  document.getElementById('Homepage').style.display = 'none';
-  document.getElementById('About').style.display = 'none';
-  document.getElementById('contactInfo').style.display = 'block';
-}
+const showPage = (pageId) => {
+  const pages = [ 'Homepage', 'About', 'contactInfo' ];
+  pages.forEach((page) => {
+    const display = page === pageId ? 'block' : 'none';
+    document.getElementById(page).style.display = display;
+  });
+};
+
+const showHomePage = () => {
+  showPage('Homepage');
+};
+
+const showAboutPage = () => {
+  showPage('About');
+};
+
+const showContactInfo = () => {
+  showPage('contactInfo');
+};
